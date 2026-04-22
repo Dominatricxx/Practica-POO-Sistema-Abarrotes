f8103ca49d69483ef9c8a7916808c417e2869403
rochaedgar-max <rocha.edgar@uabc.edu.mx>
2026-04-21 21:31:10 -0700

    fix: arreglé unos problemas con los nombres

--- model/producto.py
+++ model/producto.py
@@ -2,9 +2,9 @@ from abc import ABC, abstractmethod
 
 class Producto(ABC):
     def __init__(self, codigoBarra, nombre, categoria, precioCompra, precioVenta, stock):
-        self.odigoBarra = codigoBarra
-        self.ombre = nombre
-        self.ategoria = categoria
+        self.codigoBarra = codigoBarra
+        self.nombre = nombre
+        self.categoria = categoria
         self.precioCompra = precioCompra
         self.precioVenta = precioVenta
         self.__stock = stock
